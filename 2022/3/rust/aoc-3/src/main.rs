325cb63858d2d76cded5a846f900c5f658e43ea9
Hubert <h.weydmann@plusautomatyka.pl>
2022-12-05 17:25:56 +0100

    aoc3p2 in progress

--- 2022/3/rust/aoc-3/src/main.rs
+++ 2022/3/rust/aoc-3/src/main.rs
@@ -1,13 +1,9 @@
 
-use std::fs;
+use std::{fs, thread::current};
 
 fn main() {
-    println!("Hello, world!");
-
+    
     let file_path = "../../puzzle_input.txt";
-
-    println!("In file {}", file_path);
-
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
         
@@ -16,8 +12,12 @@ fn main() {
     
     
 
-    let mut priority_sum: i32 = 0;
+    let mut priority_sum_answer1: i32 = 0;
+    let mut line_number: u16 = 0;
+    let mut current_elf_group_line = String::from("");
+    
     for line in trimmed.split("\n") {
+        line_number = line_number + 1;
         let line_length = line.len();
         assert!(line_length % 2 == 0);
 
@@ -29,8 +29,8 @@ fn main() {
             for char2 in halves.1.chars() {
                 if char1 == char2 {
                     let priority = item_priority(char1);// - 0x48;
-                    priority_sum = priority_sum + priority as i32;
-                    println!("{line} : {priority}");
+                    priority_sum_answer1 = priority_sum_answer1 + priority as i32;
+                    //println!("{line} : {priority}");
                     char_found = true;
                     break;
                 }
@@ -39,14 +39,29 @@ fn main() {
                 break;
             }
         }
+
+        if line_number <= 3 {
+            current_elf_group_line.push_str(line);
+        }
+
+        if line_number == 3 {
+            line_number = 0;
+            let collected_lines = current_elf_group_line;
+            current_elf_group_line = String::from("");
+            println!("{collected_lines}");
+        }
+
     }
-    println!("Puzzle answer 1: {priority_sum}");
+    println!("Puzzle answer part 1:\n{priority_sum_answer1}\n");
+
+
+
 
 }
 
 fn item_priority(item: char) -> u8 {
     let b = item as u8;
-    if (b >= 97) {
+    if b >= 97 {
         b - 96        
     } else {
         b - 64 + 26
