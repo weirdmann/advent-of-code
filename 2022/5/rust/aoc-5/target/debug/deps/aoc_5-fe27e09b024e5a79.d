6991afabb20d6bc8c0491d53e89faeb2efc2e3d4
Hubert <h.weydmann@plusautomatyka.pl>
2022-12-06 01:04:32 +0100

    aoc5 start

---
+++ 2022/5/rust/aoc-5/target/debug/deps/aoc_5-fe27e09b024e5a79.d
@@ -0,0 +1,5 @@
+x:\Coding\advent-of-code\2022\5\rust\aoc-5\target\debug\deps\aoc_5-fe27e09b024e5a79.rmeta: src\main.rs
+
+x:\Coding\advent-of-code\2022\5\rust\aoc-5\target\debug\deps\aoc_5-fe27e09b024e5a79.d: src\main.rs
+
+src\main.rs:
