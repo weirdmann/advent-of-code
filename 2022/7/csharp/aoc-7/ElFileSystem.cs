b5941ac556d7928492af53c7c3fb32a753777efd
Hubert <h.weydmann@plusautomatyka.pl>
2022-12-12 12:35:47 +0100

    Update ElFileSystem.cs

--- 2022/7/csharp/aoc-7/ElFileSystem.cs
+++ 2022/7/csharp/aoc-7/ElFileSystem.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.CompilerServices;
+﻿
 using System.Text;
-using System.Threading.Tasks;
 
 namespace aoc_7
 {
