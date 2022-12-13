d2115b700d4f4430ffedab407d1dd6b7f316a75b
Hubert <hubwe@pm.me>
2022-12-13 20:47:46 +0100

    aoc-9 update

--- 2022/9/rust/aoc-9/src/main.rs
+++ 2022/9/rust/aoc-9/src/main.rs
@@ -8,20 +8,33 @@ fn main() {
     let mut head_position = (0,0);
     let mut tail_position = (0,0);
     let mut visited : HashMap<(i32,i32), i32> = HashMap::new();
+    visited.insert(tail_position.clone(), 0);
 
     for line in lines {
         let m = applyMove(line);
-        println!("{:?}", &m);
-        for i in 0..=m.2 {
+        //println!("{:?}", &m);
+        for i in 1..=m.2 {
             let new_head_position = (&head_position.0 + &m.0, &head_position.1 + &m.1);
             let head_tail_difference = (&new_head_position.0 - &tail_position.0, &new_head_position.1 - &tail_position.1);
-            if head_tail_difference.0.abs().gt(&1) || head_tail_difference.1.abs().ge(&1)  {
-                println!("move tail: {:?}", &head_tail_difference);
-                tail_position = head_position;
-
-            }
+            if head_tail_difference.0.abs().gt(&1) || head_tail_difference.1.abs().gt(&1)  {
+                //println!("move tail: {:?}", &head_tail_difference);
+                tail_position = head_position.clone();
+                match visited.insert(tail_position.clone(), 0) {
+                    Some(_) => {},//println!("new"),
+                    None => {}//println!("old")
+                };
+            }// } else {
+            //     if &new_head_position == &tail_position {
+            //         println!("covers");
+            //     }
+            //     //println!("no change: {:?}{:?}", &new_head_position, &tail_position);
+            // }
+            head_position = new_head_position;
         }
+
     }
+
+    println!("answer1: {}", visited.len())
 }
 
 fn applyMove(mv : &str) -> (i32,i32, i32) {
