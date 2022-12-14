ba418dd9cbd9d2f5259c8c512b0f3eec8379d69f
Hubert <hubwe@pm.me>
2022-12-14 23:01:17 +0100

    aoc-9 in rust FINALLY DONE

--- 2022/9/rust/aoc-9/src/main.rs
+++ 2022/9/rust/aoc-9/src/main.rs
@@ -5,41 +5,17 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let lines = contents.lines();
 
-    // let mut head_position = (0, 0);
-    // let mut tail_position = (0, 0);
-    // let mut visited: HashMap<(i32, i32), i32> = HashMap::new();
-    // visited.insert(tail_position.clone(), 0);
-
-    // for line in lines.clone() {
-    //     let m = parse_move(line);
-    //     for _ in 1..=m.2 {
-    //         let new_head_position = (&head_position.0 + &m.0, &head_position.1 + &m.1);
-    //         let head_tail_difference = (
-    //             &new_head_position.0 - &tail_position.0,
-    //             &new_head_position.1 - &tail_position.1,
-    //         );
-    //         if head_tail_difference.0.abs().gt(&1) || head_tail_difference.1.abs().gt(&1) {
-    //             tail_position = head_position.clone();
-    //             match visited.insert(tail_position.clone(), 0) {
-    //                 Some(_) => {}
-    //                 None => {}
-    //             }
-    //         }
-    //         head_position = new_head_position;
-    //     }
-    // }
-    // let answer1 = &visited.len();
-    // println!("answer1: {}", answer1);
-
     let mut head = Knot::new();
     for _ in 1..=9 {
         head.add_knot();
     }
     for line in lines.clone() {
-        //println!("{}", line);
         head.apply_move(line);
     }
-    println!("{}", head.get_visited());
+    println!(
+        "{}",
+        head.get_visited()
+    );
 }
 
 fn parse_move(mv: &str) -> (i32, i32, i32) {
@@ -87,7 +63,9 @@ impl Knot {
     fn get_visited(&self) -> String {
         match &self.next {
             None => format!("{}", self.visited.len()),
-            Some(next) => format!("{},\n{}", self.visited.len(), next.get_visited()),
+            Some(next) => {
+                format!("{},\n{}", self.visited.len(), next.get_visited())
+            }
         }
     }
 
@@ -100,52 +78,40 @@ impl Knot {
                 y: &self.position.y + &m.1,
             };
 
-            match self.next.as_mut() {
-                Some(next) => next.follow(&new_head_position.clone(), &self.position.clone()),
-                None => {}
-            }
             self.position.mv(&new_head_position);
             self.visited.insert(self.position.as_tuple(), false);
 
-            // match self.next.as_mut() {
-            //     None => self.position.mv(&new_head_position),
-            //     Some(next) => {
-            //         let head_tail_difference = Position {
-            //             x: &new_head_position.x - &next.position.x,
-            //             y: &new_head_position.y - &next.position.y,
-            //         };
-            //         if head_tail_difference.x.abs().gt(&1) || head_tail_difference.y.abs().gt(&1) {
-            //             next.position.mv(&self.position);
-            //             match self.visited.insert(self.position.as_tuple(), false) {
-            //                 Some(_) => {}
-            //                 None => {}
-            //             }
-            //         }
-            //         self.position.mv(&new_head_position);
-            //     }
-            // }
+            match self.next.as_mut() {
+                Some(next) => next.follow(&self.position.clone()),
+                None => {}
+            }
+
         }
     }
 
-    fn follow(&mut self, new_head_position: &Position, old_head_position: & Position) {
+    fn follow(&mut self, new_head_position: &Position) {
         let head_tail_difference = Position {
             x: &new_head_position.x - &self.position.x,
             y: &new_head_position.y - &self.position.y,
         };
 
-        //println!("x:{} y:{}", &head_tail_difference.x, &head_tail_difference.y);
-
         if head_tail_difference.x.abs().gt(&1) || head_tail_difference.y.abs().gt(&1) {
+
+            let mut d : (i32,i32) = (0,0);
+            if head_tail_difference.x != 0 {d.0 = head_tail_difference.x / head_tail_difference.x.abs();}
+            if head_tail_difference.y != 0 {d.1 = head_tail_difference.y / head_tail_difference.y.abs();}
+
+            self.position.mv_by(d.0, d.1);
+            self.visited.insert(self.position.as_tuple(), false);
+
             match self.next.as_mut() {
                 Some(next) => {
-                    next.follow(&old_head_position.clone(), &self.position.clone());
+                    next.follow(&self.position.clone());
                 }
                 None => {}
             }
-
-            self.position.mv(&old_head_position);
-            self.visited.insert(self.position.as_tuple(), false);
         }
+
     }
 }
 
@@ -163,5 +129,10 @@ impl Position {
     fn mv(&mut self, pos: &Position) {
         self.x = pos.x;
         self.y = pos.y;
+    }
+
+    fn mv_by(&mut self, dx: i32, dy: i32) {
+        self.x = self.x + dx;
+        self.y = self.y + dy;
     }
 }
