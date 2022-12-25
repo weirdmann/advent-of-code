b3c0544086993bbd96630061095efa549119cc42
Hubert <h.weydmann@plusautomatyka.pl>
2022-12-25 18:46:18 +0100

    Update Program.cs

--- 2022/--- Day 13 Distress Signal ---/csharp/aoc-13/Program.cs
+++ 2022/--- Day 13 Distress Signal ---/csharp/aoc-13/Program.cs
@@ -1,3 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections;
+
 Console.WriteLine("Hello, World!");
+
+int[] a = { 1, 2, 3 };
+int[] b = { 1, 2, 3 };
+Console.WriteLine($"{Packet.Comparray(a, b)}");
+
+public struct Packet
+{
+    public List<Packet> Array;
+    public enum CompareResult
+    {
+        OutOfOrder,
+        InOrder
+    }
+
+    public static CompareResult Comparray(int[] shouldBeSmaller, int[] shouldBeBigger)
+    {
+        if (shouldBeBigger.Length != shouldBeSmaller.Length)
+        {
+            return shouldBeBigger.Length > shouldBeSmaller.Length
+                ? CompareResult.InOrder
+                : CompareResult.OutOfOrder;
+        }
+
+        return shouldBeSmaller.Where((t, i) => shouldBeBigger[i] < t).Any()
+            ? CompareResult.OutOfOrder
+            : CompareResult.InOrder;
+    }
+
+    public static object ParseLine(string line)
+    {
+        const char OPENING = '[';
+        const char CLOSING = ']';
+        var FoundOpened = 0;
+        var CurrentLine = "";
+        
+        foreach (var c in line)
+        {
+            
+        }
+        
+        
+        return null;
+
+    }
+}
